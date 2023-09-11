eb1a370901e27d8cf3b50f1876f40755649fa263
Tomah <959104@cognizant.com>
2023-09-10 22:09:41 -0500

    Delete Login

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {
+
     public static void mainMenu(){
         Scanner scan = new Scanner(System.in);
 
@@ -12,7 +13,7 @@ public class Menu {
 
         switch (userInput){
             case 1:
-                Login.login();
+                login();
                 break;
             case 2:
                 System.out.println("Goodbye");
@@ -22,6 +23,25 @@ public class Menu {
                 System.out.println("Invalid Input");
                 mainMenu();
         }
+
+
+
+
+        }
+    public static void login(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter username");
+        String username = scan.nextLine();
+        System.out.println("Enter password");
+        String password = scan.nextLine();
+
+        if (username.equals("user") && password.equals("123")) {
+            System.out.println("Welcome to your Account, User");
+        } else {
+            System.out.println("Wrong username and/or password");
+            login();
+        }
+
 
     }
 }
